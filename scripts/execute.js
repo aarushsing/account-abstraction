91959c877567cc228aacfd4f292a5e348eb48fb7
Aarush singh <aarushs505@gmail.com>
2024-09-05 13:19:57 +0530

    added sender and nonce in userOp

--- scripts/execute.js
+++ scripts/execute.js
@@ -1,11 +1,22 @@
 const hre = require("hardhat");
+const FACTORY_NONCE = 1;
+
+const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 
 async function main() {
-  const ep = await hre.ethers.deployContract("EntryPoint");
+  const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);
+
+
+  const sender = await hre.ethers.getCreateAddress({
+    from : FACTORY_ADDRESS,
+    nonce : FACTORY_NONCE
+  })
 
   const userOP = {
      sender,
-     nonce,
+     nonce: await entryPoint.getNonce(sender,0),
      initCode,
      callData,
      callGasLimit: 200_000,
