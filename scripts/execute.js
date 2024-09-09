bbd79644f3d7185b7a4a70127e8e22e886a174e1
Aarush singh <aarushs505@gmail.com>
2024-09-09 16:06:14 +0530

    changes in addressess

--- scripts/execute.js
+++ scripts/execute.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 const FACTORY_NONCE = 1;
 
-const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const FACTORY_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 
@@ -11,8 +11,8 @@ async function main() {
 
   const sender = await hre.ethers.getCreateAddress({
     from : FACTORY_ADDRESS,
-    nonce : FACTORY_NONCE
-  })
+    nonce : FACTORY_NONCE,
+  });
 
   //signer (owner) , we gonna use as an argument which will be our calldata
   const [signer] = await hre.ethers.getSigners();  // it will get us array of accounts
@@ -21,13 +21,15 @@ async function main() {
 
   const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
 
-  const initCode = FACTORY_ADDRESS + AccountFactory.interface.encodeFunctionData("createAccount",[address0]);
+  const initCode = FACTORY_ADDRESS + AccountFactory.interface.encodeFunctionData("createAccount",[address0]).slice(2);
+
+  const Account = await hre.ethers.getContractFactory("Account");
 
   const userOP = {
      sender,
      nonce: await entryPoint.getNonce(sender,0),
      initCode,
-     callData,
+     callData: Account.interface.encodeFunctionData("execute"),
      callGasLimit: 200_000,
      verificationGasLimit: 200_000,
      preVerificationGas: 50_000,
@@ -36,6 +38,10 @@ async function main() {
      paymasterAndData:"0x",
      signature:"0x"
   }
+
+  const tx = await entryPoint.handleOps([userOP], address0);
+  const receipt = await tx.wait();
+  console.log(receipt);
 
 }
 
